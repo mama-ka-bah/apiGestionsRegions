230aa968cbbf0db5dbe7be2c2af5ca0e2e11aafd
KEITA Mahamadou <kmahamadou858@gmail.com>
2022-08-11 16:48:48 +0000

    ajout du projet

---
+++ src/main/java/com/api/apiRegion/ApiRegionApplication.java
@@ -0,0 +1,13 @@
+package com.api.apiRegion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRegionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRegionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/apiRegion/ApiRegionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.apiRegion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRegionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
