c48f48580604046ccf0392c4f4de53701ca266a1
KEITA Mahamadou <kmahamadou858@gmail.com>
2022-08-12 14:09:03 +0000

    karim

---
+++ src/main/java/com/api/apiRegion/services/paysSeImplement.java
@@ -0,0 +1,38 @@
+package com.api.apiRegion.services;
+
+import com.api.apiRegion.modele.Pays;
+import com.api.apiRegion.repository.paysRepository;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+@AllArgsConstructor
+
+public class paysSeImplement implements paysServices {
+
+    private  paysRepository paysrepository;
+
+    @Override
+    public Pays creer(Pays pays) {
+        return paysrepository.save(pays);
+    }
+
+    @Override
+    public List<Pays> lire() {
+        return paysrepository.findAll();
+    }
+
+    @Override
+    public Pays modifier(Long id, Pays pays) {
+        return paysrepository.findById(id)
+                .map(r->{
+                    r.setNom(pays.getNom());
+                    return paysrepository.save(r);
+                }).orElseThrow(() -> new RuntimeException("Pays nom trouver"));
+    }
+
+    @Override
+    public String supprimer(Long id) {
+        paysrepository.deleteById(id);
+        return "Pays Supprimer";
+    }
+}
