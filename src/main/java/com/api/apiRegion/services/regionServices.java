088950ce478415b1c00b71c5dbf49c4cf94294a7
Dmchez33 <iddembele206@gmail.com>
2022-08-14 11:09:24 +0000

    COMMENTEUR DE QUELQUES CODE

--- src/main/java/com/api/apiRegion/services/regionServices.java
+++ src/main/java/com/api/apiRegion/services/regionServices.java
@@ -5,10 +5,18 @@ import com.api.apiRegion.modele.Regions;
 import java.util.List;
 
 public interface regionServices {
-
+    //methode permettant de creer une region
     Regions creer(Regions regions);
+    //methode permettant de lister une region
     List<Object[]> lire();
+    //methode permettant de lister des regions sans pays
     Iterable<Object[]> lireSansPays();
+    //methode permettant de lister des regions avec l'évolution
+    List<Object[]> lireRegionHbtAnnee();
+    //methode permettant de lister les regions d'un pays
+    List<Object[]> lireRegionOfPays(String pays);
+    //methode permettant de modifier une region
     Regions modifier(Long id, Regions regions);
+    //methode permettant de de supprimer une region
     String supprimer(Long id);
 }
